34915a22115bafcfaa7602fe43bf7eb044673222
Afaan Bilal <afaanbilal@gmail.com>
2023-08-16 11:32:20 +0530

    ✨ Basic setup

---
+++ OpenGLTestbench/src/OpenGLTestbench.cpp
@@ -0,0 +1,17 @@
+/**
+ * OpenGL Testbench
+ *
+ * @author Afaan Bilal
+ * @link   https://afaan.dev
+ * @link   https://github.com/AfaanBilal/OpenGLTestbench
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "OpenGL Testbench" << std::endl;
+	std::cout << "(c) Afaan Bilal - https://afaan.dev" << std::endl;
+
+	std::cin.get();
+}
