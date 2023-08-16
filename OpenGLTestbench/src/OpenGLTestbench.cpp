d4afde2eac8899fb90db57a03e02bcb46b9fc506
Afaan Bilal <afaanbilal@gmail.com>
2023-08-16 12:13:45 +0530

    ✨ Basic triangle - no shaders

--- OpenGLTestbench/src/OpenGLTestbench.cpp
+++ OpenGLTestbench/src/OpenGLTestbench.cpp
@@ -10,6 +10,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "OpenGLTestbench.h"
+
 int main()
 {
     std::cout << "OpenGL Testbench" << std::endl;
@@ -20,7 +22,7 @@ int main()
     if (!glfwInit())
         return -1;
 
-    window = glfwCreateWindow(640, 480, "OpenGL Testbench", NULL, NULL);
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE, NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -36,9 +38,25 @@ int main()
     }
     std::cout << "GLEW: " << glewGetString(GLEW_VERSION) << std::endl;
 
+    float vertices[] = {
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+         0.0f,  0.5f,
+    };
+
+    GLuint vb;
+    glGenBuffers(1, &vb);
+    glBindBuffer(GL_ARRAY_BUFFER, vb);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
--- OpenGLTestbench/src/OpenGLTestbench.h
+++ OpenGLTestbench/src/OpenGLTestbench.h
@@ -1,1 +1,12 @@
 #pragma once
+/**
+ * OpenGL Testbench
+ *
+ * @author Afaan Bilal
+ * @link   https://afaan.dev
+ * @link   https://github.com/AfaanBilal/OpenGLTestbench
+ */
+
+#define SCREEN_WIDTH  600
+#define SCREEN_HEIGHT 600
+#define WINDOW_TITLE  "OpenGL Testbench"
