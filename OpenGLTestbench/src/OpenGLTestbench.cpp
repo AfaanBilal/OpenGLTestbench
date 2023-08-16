a21c8fcd890562cfbcd1333dcd87892db576b11b
Afaan Bilal <afaanbilal@gmail.com>
2023-08-16 11:46:27 +0530

    ✨ GLFW window setup

--- OpenGLTestbench/src/OpenGLTestbench.cpp
+++ OpenGLTestbench/src/OpenGLTestbench.cpp
@@ -7,11 +7,36 @@
  */
 
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	std::cout << "OpenGL Testbench" << std::endl;
-	std::cout << "(c) Afaan Bilal - https://afaan.dev" << std::endl;
+    std::cout << "OpenGL Testbench" << std::endl;
+    std::cout << "(c) Afaan Bilal - https://afaan.dev" << std::endl;
 
-	std::cin.get();
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "OpenGL Testbench", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
