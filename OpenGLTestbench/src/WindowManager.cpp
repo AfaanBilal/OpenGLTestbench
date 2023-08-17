23ee1992046d29bb0120579fefea0cb1275617d0
Afaan Bilal <afaanbilal@gmail.com>
2023-08-17 12:33:44 +0530

    ✨ Enable blending

--- OpenGLTestbench/src/WindowManager.cpp
+++ OpenGLTestbench/src/WindowManager.cpp
@@ -35,6 +35,9 @@ GLFWwindow* WindowManager::Initialize()
 	}
 	std::cout << "GLEW: " << glewGetString(GLEW_VERSION) << std::endl;
 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	return window;
 }
 
